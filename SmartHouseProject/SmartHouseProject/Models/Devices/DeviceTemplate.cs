91dcb7a5a86175da7d78d2369d2b03c3cccf7ed2
AnesCenanovic <acenanovic1@etf.unsa.ba>
2025-02-09 16:34:17 +0100

    Dodavanje aspekata praćenja potrošnje električne energije

--- SmartHouseProject/SmartHouseProject/Models/Devices/DeviceTemplate.cs
+++ SmartHouseProject/SmartHouseProject/Models/Devices/DeviceTemplate.cs
@@ -11,20 +11,44 @@ namespace SmartHouseProject.Models.Devices
     {
         public string Name { get; protected set; } 
         public bool State { get; protected set; } // is on or off
-
-        public DeviceTemplate(string name) {
+        public double PowerConsumption { get; protected set; } // watts
+        public double PowerUsage {  get; protected set; } // usage for later bill calculation
+        public DateTime? TimeSpentActive { get; protected set; } // total time spent consuming electricity
+        public DeviceTemplate(string name,double powerConsumption) {
             Name = name;
+            PowerConsumption = powerConsumption;
             State = false; // off by default
+            PowerUsage = 0;
         }
 
         public void TurnOn()
         {
-            State = true;
+            if (!State)
+            {
+                State = true;
+                TimeSpentActive = DateTime.Now;
+            }
+            else
+            {
+                Console.WriteLine("Device already on.");
+            }
         }
-        public void TurnOff() { 
-            State = false; 
+        public void TurnOff() {
+            if (State)
+            {
+                State = false;
+                if (TimeSpentActive.HasValue)
+                {
+                    TimeSpan timeActive = DateTime.Now - TimeSpentActive.Value;
+                    PowerUsage += timeActive.TotalHours * PowerUsage; // logic for calculating power usage (in watts per hour)
+                }
+            }
+            else
+            {
+                Console.WriteLine("Device already off.");
+            }
         }
 
-        public abstract void StatusReport(); // za printanje detalja pojedinačnog uređaja
+        public abstract void StatusReport(); // za printanje detalja pojedinačnog uređaja, ovo implementiraju izvedene klase 
     }
 }
